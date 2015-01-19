55ff66d0c89d9108504da1dccfb012055b1f79d7
donghyuc kim <kkimluv+1@gmail.com>
2015-01-19 16:03:50 +0900

    Update post_list.js

--- post_list.js
+++ post_list.js
@@ -5,15 +5,7 @@ var post_list = {
     "posts": [
       null
       {% for post in site.posts %}
-        ,{
-            "title"    : "{{ post.title }}",
-            "href"     : "{{ post.url }}",
-            "date"     : {
-                "day"   : "{{ post.date | date: "%d" }}",
-                "month" : "{{ post.date | date: "%B" }}",
-                "year"  : "{{ post.date | date: "%Y" }}"
-            }
-        }
+        ,{% include post.json %}
       {% endfor %}
     ]
 };
