6364c608b96ebc1c37c25e70ae021ad7691de93b
donghyuc kim <kkimluv+1@gmail.com>
2015-01-19 16:41:44 +0900

    Update post_list.js

--- post_list.js
+++ post_list.js
@@ -1,11 +1,15 @@
 ---
 ---
 
-var post_list = {
+var post_list_text = '{
     "posts": [
       null
       {% for post in site.posts %}
         ,{% include post.json %}
       {% endfor %}
     ]
-};
+}';
+
+post_list_text = post_list_text.replace(/¥n/g, ''); 
+
+var post_list = JSON.parse(post_list_text; 
