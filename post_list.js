32f62d121c7ad6d5cce5bbd41256096fab9d4d69
donghyuc kim <kkimluv+1@gmail.com>
2015-01-19 15:56:22 +0900

    Create post_list.js

---
+++ post_list.js
@@ -0,0 +1,19 @@
+---
+---
+
+var post_list = {
+    "posts": [
+      null
+      {% for post in site.posts %}
+        ,{
+            "title"    : "{{ post.title }}",
+            "href"     : "{{ post.url }}",
+            "date"     : {
+                "day"   : "{{ post.date | date: "%d" }}",
+                "month" : "{{ post.date | date: "%B" }}",
+                "year"  : "{{ post.date | date: "%Y" }}"
+            }
+        }
+      {% endfor %}
+    ]
+};
